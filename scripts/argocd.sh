7eda7b9b399ffa9bf6fbc8ef43d03e34010ae4b2
buicongthanh861 <buicongthanh861@gmail.com>
2026-08-21 00:51:23 +0700

    final cicd

--- scripts/argocd.sh
+++ scripts/argocd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "ArgoCD: http://localhost:8080"
+echo "🔐 ArgoCD: http://localhost:8080"
 echo "Username: admin"
 echo "Password: $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d)"
+echo ""
+echo "Press Ctrl+C to stop"
 kubectl -n argocd port-forward svc/argocd-server 8080:443
